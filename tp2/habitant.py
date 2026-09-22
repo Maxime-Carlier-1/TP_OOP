4268f1ae16e479c7372012901ae3e0d95e5dd471
Carlier Maxime <maxime.carlier314@gmail.com>
2026-09-22 14:32:42 +0200

    Question 2 : methode class habitant

--- tp2/habitant.py
+++ tp2/habitant.py
@@ -9,3 +9,17 @@ class Habitant():
         self.adresse = adresse
         self.animaux = animaux if animaux is not None else {}
 
+    def affichage_adresse(self):
+        '''Affche l'adresse de l'habitant'''
+        print(f"{self.nom} habite a {self.adresse}")
+
+    def compte_animal(self, animal):
+        '''Return le compte des animaux (0 si l'animal n'existe pas)'''
+        return self.animaux.get(animal, 0)
+
+
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+assert h1.nom == "Aldric"
+assert h1.compte_animal("vaches") == 3
+assert h1.compte_animal("moutons") == 0
+h1.affichage_adresse() # affiche "Aldric habite a Rue A"
