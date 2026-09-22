794c87e48967560ebd71950f620eb48c3f8d9661
Carlier Maxime <maxime.carlier314@gmail.com>
2026-09-22 16:52:56 +0200

    Exercice 9 Question 2 : test class village

--- tp2/tests.py
+++ tp2/tests.py
@@ -2,6 +2,7 @@
 
 import unittest
 from habitant import *
+from village import *
 
 class TestHabitant(unittest.TestCase):
     """Tests pour la classe Habitant et l’encapsulation."""
@@ -23,6 +24,35 @@ class TestHabitant(unittest.TestCase):
         """Cas limite : animal inexistant"""
         habitant = Adulte("Alice", 30, "Rue C", {"vache": 2})
         self.assertEqual(habitant.compte_animal("cheval"), 0)
+
+class TestVillage(unittest.TestCase):
+    """Tests pour la classe Village et l’agregation/composition."""
+
+    def test_composition_valide(self):
+        village = Village("TestVillage")
+        village.ajouter_habitant_composition("Bob", 40, "Rue E", None)
+        #on compare les attributs ca rsi on crée un habitant par composition, il n'est pas accessible en dehors du village
+        #et donc on ne peut pas le verifier dans assertEqual
+        self.assertEqual(len(village.habitants), 1)
+        self.assertEqual(village.habitants[0].get_nom(), "Bob")
+        self.assertEqual(village.habitants[0].age, 40)
+        self.assertEqual(village.habitants[0].get_adresse(), "Rue E")
+        self.assertEqual(village.habitants[0].get_animaux(), {})
+
+    def test_agregation_valide(self):
+        village = Village("TestVillage")
+        h = Adulte("Bob", 40, "Rue E", None)
+        village.ajouter_habitant_agregation(h)
+        self.assertEqual(village.habitants, [h])
+
+    def test_agregation_partagee(self):
+        village1 = Village("Village1")
+        village2 = Village("Village2")
+        h = Adulte("Bob", 40, "Rue E", None)
+        village1.ajouter_habitant_agregation(h)
+        village2.ajouter_habitant_agregation(h)
+        self.assertIn(h, village1.habitants)
+        self.assertIn(h, village2.habitants)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
