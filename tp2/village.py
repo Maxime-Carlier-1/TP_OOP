5ebec1b658845b9c8ac829aa58041456fee79487
Carlier Maxime <maxime.carlier314@gmail.com>
2026-09-22 15:06:28 +0200

    Exercice 5 Question 2 : ajout des methodes

--- tp2/village.py
+++ tp2/village.py
@@ -1,8 +1,41 @@
 """Exercices 2 -- Village"""
 
+from habitant import Habitant
+
 class Village():
     '''Classe village'''
 
     def __init__(self, nom):
         self.nom = nom
         self.habitants = []
+
+    def get_habitants(self):
+        '''Return la liste des habitants'''
+        return self.habitants
+
+    def ajouter_habitant_composition(self, nom, age, adresse, animaux=None):
+        '''Ajoute un habitant par composition'''
+        habitant = Habitant(nom, age, adresse, animaux)
+        self.habitants.append(habitant)
+
+    def ajouter_habitant_agregation(self, habitant):
+        '''Ajoute un habitant par agregation'''
+        self.habitants.append(habitant)
+
+    def afficher_habitants(self):
+        '''Affiche la liste des habitants'''
+        for h in self.habitants:
+            print(h)
+
+
+pytown = Village("PyTown")
+pytown.ajouter_habitant_composition("Aldric", 25, "Rue A", {"vaches": 3})
+
+elise = Habitant("Elise", 28, "Rue B", {"poules": 10})
+pytown.ajouter_habitant_agregation(elise)
+
+autre_village = Village("VillageVoisin")
+autre_village.ajouter_habitant_agregation(elise) # meme habitant dans 2 villages
+
+assert len(pytown.get_habitants()) == 2
+assert elise in autre_village.get_habitants()
